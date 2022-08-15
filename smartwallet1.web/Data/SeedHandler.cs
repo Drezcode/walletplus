9e0dc8d5620054db4562db2f0a067e9b8a12384b
Andrew Ogbeiwi <andrewogbeiwi92@gmail.com>
2022-08-15 15:13:35 +0100

    first commit

---
+++ smartwallet1.web/Data/SeedHandler.cs
@@ -0,0 +1,37 @@
+﻿using smartwallet1.web.Data.Entities;
+
+namespace smartwallet1.web.Data
+{
+    public class    SeedHandler
+    {
+        public static async Task Seed(ApplicationDbContext context)
+        {
+            //Check if the database is populated
+            if (!context.Customers.Any())
+            {
+                //Create Sample Data
+                context.Customers.Add(new Customer
+                {
+                    FirstName = "Andrew",
+                    LastName = "Ogbeiwi",
+                    MiddleName  = "Chike",
+                    Country = "Nigeria",
+                    State  = "Lagos",
+                    City = "Alimosho",
+                    BirthDate = DateTime.Now.AddYears(-2),
+                    Gender = Gender.Male,
+                    Status = MaritalStatus.single,
+                   
+                });
+
+                
+                await context.SaveChangesAsync();
+            }
+
+           
+
+            
+        }
+    }
+}
+
